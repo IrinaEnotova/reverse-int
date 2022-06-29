84f5b9440fb8bdae9bc4a24f492112cc84063957
Irina Akhanteva <irinkayashina@gmail.com>
2022-06-29 00:47:35 -1100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let nString = n.toString();
+  let i = nString.length;
+  let result = '';
+
+  while(i > 0) {
+    result += nString[i-1];
+    console.log(result);
+    i--;
+  }
+
+  return +result;
 }
